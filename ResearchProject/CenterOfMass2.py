4dccc8cf5ed631889bde2de846407010d3fb1cd5
wwalkerrockwell <57078388+wwalkerrockwell@users.noreply.github.com>
2020-04-29 16:36:22 -0700

    Added Code, txt files, and graphs

---
+++ ResearchProject/CenterOfMass2.py
@@ -0,0 +1,193 @@
+# Homework 4
+# Center of Mass Position and Velocity
+# William Rockwell
+
+
+# import modules
+import numpy as np
+import math
+import astropy.units as u
+from Readfile import Read
+
+
+class CenterOfMass2:
+# Class to define COM position and velocity properties of a given galaxy 
+# and simulation snapshot
+    
+    
+    def __init__(self, filename, ptype):
+    # Initialize the instance of this Class with the following properties:
+    
+        # read data in the given file using Read
+        self.data, self.time, self.total  = Read(filename)                                                                                             
+
+        #create an array to store indexes of particles of desired Ptype                                
+        self.index = np.where(self.data['type'] == ptype)
+
+        # store the mass, positions, velocities of only the particles of the given type
+        # the following only gives the example of storing the mass
+        self.m = self.data['m'][self.index]
+        # write your own code to complete this for positions and velocities
+        self.x = self.data['x'][self.index]
+        self.y = self.data['y'][self.index]
+        self.z = self.data['z'][self.index]
+        self.vx = self.data['vx'][self.index]
+        self.vy = self.data['vy'][self.index]
+        self.vz = self.data['vz'][self.index]
+
+        
+
+
+    def COMdefine(self,a,b,c,m):
+    # Function to compute the center of mass position or velocity generically
+    # input: array (a,b,c) of positions or velocities and the mass
+    # returns: 3 floats  (the center of mass coordinates)
+        
+        # write your own code to compute the generic COM using Eq. 1 in the homework instructions
+        # xcomponent Center of mass
+        Acom = np.sum(a*m)/np.sum(m)
+        # ycomponent Center of mass
+        Bcom = np.sum(b*m)/np.sum(m)
+        # zcomponent Center of mass
+        Ccom = np.sum(c*m)/np.sum(m)
+
+        return Acom, Bcom, Ccom
+    
+    
+    def COM_P(self, VolDec, delta):
+    # Function to specifically return the center of mass position and velocity                                         
+    # input:                                                                                                           
+    #        particle type (1,2,3)                                                                                     
+    #        delta (tolerance)                                                                                         
+    # returns: One vector, with rows indicating:                                                                                                                                                                            
+    #       3D coordinates of the center of mass position (kpc)                                                             
+
+        # Center of Mass Position                                                                                      
+        ###########################                                                                                    
+        # Try a first guess at the COM position by calling COMdefine   
+        XCOM, YCOM, ZCOM = self.COMdefine(self.x, self.y, self.z, self.m)
+        # compute the magnitude of the COM position vector.
+        # write your own code below
+
+        RCOM = np.sqrt(XCOM**2 + YCOM**2 + ZCOM**2)
+
+        # iterative process to determine the center of mass                                                            
+        # change reference frame to COM frame                                                                          
+        # compute the difference between particle coordinates                                                          
+        # and the first guess at COM position
+        # write your own code below
+        xNew = self.x - XCOM
+        yNew = self.y - YCOM
+        zNew = self.z - ZCOM
+        RNEW = np.sqrt(xNew**2 + yNew**2 + zNew**2)
+        # find the max 3D distance of all particles from the guessed COM                                               
+        # will re-start at half that radius (reduced radius)                                                           
+        RMAX = max(RNEW)/VolDec
+        
+        # pick an initial value for the change in COM position                                                      
+        # between the first guess above and the new one computed from half that volume
+        # it should be larger than the input tolerance (delta) initially
+        CHANGE = 1000.0
+
+        # start iterative process to determine center of mass position                                                 
+        # delta is the tolerance for the difference in the old COM and the new one.    
+        
+        while (CHANGE > delta):
+            # select all particles within the reduced radius (starting from original x,y,z, m)
+            # write your own code below (hints, use np.where)
+            index2 = np.where(RNEW < RMAX)
+            x2 = self.x[index2]
+            y2 = self.y[index2]
+            z2 = self.z[index2]
+            m2 = self.m[index2]
+
+            # Refined COM position:                                                                                    
+            # compute the center of mass position using                                                                
+            # the particles in the reduced radius
+            # write your own code below
+            XCOM2, YCOM2, ZCOM2 = self.COMdefine(x2, y2, z2, m2)
+            # compute the new 3D COM position
+            # write your own code below
+            RCOM2 = np.sqrt(XCOM2**2 + YCOM2**2 + ZCOM2**2)
+
+
+            # determine the difference between the previous center of mass position                                    
+            # and the new one.                                                                                         
+            CHANGE = np.abs(RCOM - RCOM2)
+            # uncomment the following line if you wnat to check this                                                                                               
+            # print ("CHANGE = ", CHANGE)                                                                                     
+
+            # Before loop continues, reset : RMAX, particle separations and COM                                        
+
+            # reduce the volume by a factor of 2 again                                                                 
+            RMAX = RMAX/VolDec
+            # check this.                                                                                              
+            #print ("maxR", maxR)                                                                                      
+
+            # Change the frame of reference to the newly computed COM.                                                 
+            # subtract the new COM
+            # write your own code below
+            xNew = self.x - XCOM2
+            yNew = self.y - YCOM2
+            zNew = self.z - ZCOM2
+            RNEW = np.sqrt(xNew**2 + yNew**2 + zNew**2)
+
+            # set the center of mass positions to the refined values                                                   
+            XCOM = XCOM2
+            YCOM = YCOM2
+            ZCOM = ZCOM2
+            RCOM = RCOM2
+
+            # create a vector to store the COM position                                                                                                                                                       
+            COMP = [XCOM, YCOM, ZCOM]
+
+        # set the correct units using astropy and round all values
+        # and then return the COM positon vector
+        # write your own code below
+        COMP *= u.kpc
+        np.around(COMP, 3)
+        return COMP
+    
+
+    def COM_V(self, COMX,COMY,COMZ):
+        # Center of Mass velocity
+        # input: X, Y, Z positions of the COM
+        # returns 3D Vector of COM Velocities
+        # the max distance from the center that we will use to determine the center of mass velocity                   
+        RVMAX = 15.0*u.kpc
+
+        # determine the position of all particles relative to the center of mass position
+        # write your own code below
+        VXCOM, VYCOM, VZCOM = self.COMdefine(self.vx, self.vy, self.vz, self.m)
+        xV = self.vx - VXCOM
+        yV = self.vy - VYCOM
+        zV = self.vz - VZCOM
+        RV = np.sqrt(xV**2 + yV**2 + zV**2) * u.kpc
+        
+        # determine the index for those particles within the max radius
+        # write your own code below
+        indexV = np.where(RV < RVMAX)
+
+        # determine the velocity and mass of those particles within the mas radius
+        # write your own code below
+        vxnew = self.vx[indexV]
+        vynew = self.vy[indexV] 
+        vznew = self.vz[indexV]
+        mnew = self.m[indexV]
+        # compute the center of mass velocity using those particles
+        # write your own code below
+        VXCOM, VYCOM, VZCOM = self.COMdefine(vxnew, vynew, vznew, mnew)
+
+        # create a vector to store the COM velocity
+        # set the correct units usint astropy
+        # round all values
+        # write your own code below
+        COMV = [VXCOM, VYCOM, VZCOM]
+        COMV *= u.km/u.s
+        np.around(COMV, 3)
+
+        # return the COM vector                                                                                        
+        return COMV
+    
+
+
---
+++ ResearchProject/DensityProfiles.py
@@ -0,0 +1,135 @@
+#What is the time evolution of the inner dark matter density profile of M33?  
+#Does it  become  more  or  less  concentrated  with  time?   
+#Is  it  well  fit  by  a  Hernquistprofile?  
+#What might it mean if there is or isn’t evolution?
+import numpy as np
+import astropy.units as u
+from astropy.constants import G
+import matplotlib.pyplot as plt
+import matplotlib
+from Readfile import Read
+from CenterOfMass2 import CenterOfMass2
+from MassProfile import MassProfile
+
+#Let galaxy be the name 
+#Let start-end be the snapshots, and n be the step interval between them
+#Let radius by an array of radii to be the profile to be passed in for each snapshot
+def DensityProfile(galaxy, start, end, n, radius):
+    fileout1 = "M33_Density.txt"
+    fileout2 = "M33_Mass.txt"
+    snapID = np.arange(start, end, n) #Array for deciding files to be read in
+    density = np.zeros([snapID.size, radius.size + 1]) #Array for containing all Profiles
+    Mhalo = np.zeros([snapID.size, radius.size + 1])
+
+    for i, snapID in enumerate(snapID): #Loop through snapshots and calculate their density profiles
+        Profile = MassProfile(galaxy, snapID)
+        Mhalo[i, 1:radius.size+1], time = Profile.MassEnclosed(1, radius) #Initialize Mass array to be plugged into density
+        print("i = ", i) 
+        Mhalo[i, 0] = time #initialize time
+        density[i, 0] = time
+
+        for j in range(np.size(radius)): #Loop through radii and calculate their local density values
+            if (j != 0):
+                density[i, j + 1] = Mhalo[i, j + 1] / ((4./3.)*3.141592*((radius[j]**3) - (radius[j - 1]**3))) * 10e2 #Calculate local density
+            else:
+                density[i, j + 1] = Mhalo[i, j + 1] / ((4./3.)*3.141592*(radius[j]**3)) * 10e2 #Calculate local density for first index
+
+    np.savetxt(fileout1, density, fmt=['%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f']) #Save density and mass arrays to files
+    np.savetxt(fileout2, Mhalo, fmt=['%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f'])
+    return
+#Each row of the file represents a snapshot; what would normally be a 1d output array of masses at varying radii
+#This means that each column represents a different radius, save for the first which is time
+#Plot out each radius as a line, evolving over time. 
+
+#a is scale length, Mhalo is the halo mass, R is the radius.
+def HernquistMass(a, Mhalo, R):
+    M = Mhalo*R**2/(R+a)**2
+    Density = (M * a) / ((2 + 3.1415 * R) * (R + a)**3)
+    return Density
+
+def HernquistDensityProfile(a, Mhalo, radius, start, end, n): #Calculate Hernquist density for each snapshot
+    snapID = np.arange(start, end, n)
+    HernquistDensity = np.zeros([snapID.size, radius.size + 1])
+
+    for i in range(np.size(snapID)): #Loop through indices of the density profiles to calculate their hernquist counterparts
+        HernquistDensity[i, 0] = Mhalo[i][0] #Initialize time
+        HernquistDensity[i, 1:radius.size+1] = HernquistMass(a, Mhalo[i][1:radius.size+1], radius) * 10e4 
+
+    np.savetxt("M33HernquistDensity.txt", HernquistDensity, fmt=['%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f']) #Save to file
+    return
+
+#This function exists so I could look at the mass profiles as well to ensure consistency
+#It uses the same inputs as HernquistDensityProfile
+#Simply change the Hernquist function to return mass instead of density to use this
+def HernquistMassProfile(a, Mhalo, radius, start, end, n): 
+    snapID = np.arange(start, end, n)
+    Mass = np.zeros([snapID.size, radius.size + 1])
+
+    for i in range(np.size(snapID)):
+        Mass[i, 0] = Mhalo[i][0]
+        Mass[i, 1:radius.size+1] = HernquistMass(a, Mhalo[i][1:radius.size+1], radius)
+
+    np.savetxt("M33HernquistMass.txt", Mass, fmt=['%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f','%.2f']) #Save to file
+    return
+
+
+radius = np.arange(1, 32, 5)
+a = 1 #Scale Factor
+#DensityProfile("M33", 0, 801, 1, radius)
+
+#Mhalo = np.loadtxt("M33_Mass.txt", skiprows=1)
+#HernquistDensityProfile(a, Mhalo, radius, 0, 801, 1)
+print(radius)
+
+"""
+Density = np.genfromtxt("M33_Density_Local.txt", dtype = None, names = True, skip_header = 0) #PLOT DENSITY PROFILES  OVER TIME
+plt.plot(Density["Time"], Density["Second"], label="R = 6kpc") 
+plt.plot(Density["Time"], Density["Third"], label="R = 11kpc")
+plt.plot(Density["Time"], Density["Fourth"], label="R = 16kpc")
+plt.plot(Density["Time"], Density["Fifth"], label="R = 21kpc")
+plt.plot(Density["Time"], Density["Sixth"], label="R = 26kpc")
+plt.plot(Density["Time"], Density["Seventh"], label="R = 31kpc")
+plt.legend()
+plt.semilogy()
+plt.xlabel("Time (Myr)")
+plt.ylabel("Density (10^2 Msun / kpc^3)") 
+plt.show()
+"""
+
+
+
+
+#HernquistDensity = np.genfromtxt("M33HernquistDensity.txt", dtype = None, names = True, skip_header = 0) #PLOT HERNQUIST PROFILES OVER TIME
+#plt.plot(HernquistDensity["Time"], HernquistDensity["Second"], label="R = Hernquist6kpc") 
+#plt.plot(HernquistDensity["Time"], HernquistDensity["Third"], label="R = Hernquist11kpc")
+#plt.plot(HernquistDensity["Time"], HernquistDensity["Fourth"], label="R = Hernquist16kpc")
+#plt.plot(HernquistDensity["Time"], HernquistDensity["Fifth"], label="R = Hernquist21kpc")
+
+
+"""
+Mhalo = np.loadtxt("M33_Mass.txt", skiprows=1) #Plot Hernquist Mass
+HernquistMass = np.loadtxt("M33HernquistMass.txt", skiprows=1)
+#HernquistMassProfile(a, Mhalo, radius, 0, 801, 1)
+plt.plot(radius, Mhalo[800][1:radius.size+1], label="Mass")
+plt.plot(radius, HernquistMass[800][1:radius.size+1], label="HernquistMass")
+
+plt.ylabel("Mass (Msun)")
+plt.xlabel("Radius (kpc)")
+plt.semilogy()
+plt.legend()
+plt.show()
+"""
+
+
+snapshot = 800#Plot critical Density Snapshots with Hernquist Profiles. Change snapshot value to see different snapshots
+Density = np.loadtxt("M33_Density_Local.txt", skiprows=1) 
+HernquistDensity = np.loadtxt("M33HernquistDensity.txt", skiprows=1)
+plt.plot(radius, Density[snapshot][1:radius.size+1], label="Density")
+plt.plot(radius, HernquistDensity[snapshot][1:radius.size+1], label="HernquistDensity")
+
+plt.ylabel("Density (10^2 Msun / kpc^3)")
+plt.xlabel("Radius (kpc)")
+plt.legend()
+plt.semilogy()
+plt.show()
+
